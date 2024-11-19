9b5a5469943335a4d9647a654833cc612ece379d
changkyu <changkyu@bejewel.kr>
2024-11-20 08:15:55 +0900

    헬퍼 추가

---
+++ appstore/Tuist/ProjectDescriptionHelpers/TargetDependacy+SPM+Extension.swift
@@ -0,0 +1,38 @@
+//
+//  TargetDependacy+SPM+Extension.swift
+//  Config
+//
+//  Created by 박창규 on 11/20/24.
+//
+
+import ProjectDescription
+
+public extension TargetDependency {
+    
+    enum SPM {
+        public static let quick: TargetDependency = .external(name: "Quick")
+        public static let nimble: TargetDependency = .external(name: "Nimble")
+        public static let snapshotTesting: TargetDependency = .external(name: "SnapshotTesting")
+        
+        public static let swinject: TargetDependency = .external(name: "Swinject")
+        
+        // UI
+        public static let snapKit: TargetDependency = .external(name: "SnapKit")
+        public static let kingfisher: TargetDependency = .external(name: "Kingfisher")
+        
+        // Rx
+        public static let rxBlocking: TargetDependency = .external(name: "RxBlocking")
+        public static let rxSwift: TargetDependency = .external(name: "RxSwift")
+        public static let rxCocoa: TargetDependency = .external(name: "RxCocoa")
+        public static let rxDataSources: TargetDependency = .external(name: "RxDataSources")
+        public static let rxGesture: TargetDependency = .external(name: "RxGesture")
+        public static let reactorKit: TargetDependency = .external(name: "ReactorKit")
+        
+        // network
+        public static let alamofire: TargetDependency = .external(name: "Alamofire")
+        public static let moya: TargetDependency = .external(name: "Moya")
+        
+        // Analytics
+    }
+}
+
---
+++ appstore/projects/Third/ReactiveX/Project.swift
@@ -0,0 +1,31 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+    name: "ReactiveX",
+    targets: [
+        .target(
+            name: "ReactiveX",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.ReactiveX",
+            deploymentTargets: .oneVersion(),
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["Sources/**"],
+            dependencies: [
+                .SPM.rxSwift,
+                .SPM.rxCocoa,
+                .SPM.rxGesture,
+                .SPM.rxDataSources,
+                .SPM.reactorKit,
+            ]
+        ),
+    ]
+)
