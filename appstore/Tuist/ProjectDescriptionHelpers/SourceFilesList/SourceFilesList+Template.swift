ac2c6c9a8e2fcf158fbd7d036d04dffaa34d6b9d
changkyu <changkyu@bejewel.kr>
2024-11-21 08:01:31 +0900

    폴더 구조 추가

--- appstore/Tuist/ProjectDescriptionHelpers/SourceFilesList/SourceFilesList+Template.swift
+++ appstore/Tuist/ProjectDescriptionHelpers/SourceFilesList/SourceFilesList+Template.swift
@@ -5,4 +5,13 @@
 //  Created by 박창규 on 11/21/24.
 //
 
-import Foundation
+import ProjectDescription
+
+public extension SourceFilesList {
+    static let demoSources: SourceFilesList = "Demo/Sources/**"
+    static let interface: SourceFilesList = "Interface/**"
+    static let sources: SourceFilesList = "Sources/**"
+    static let testing: SourceFilesList = "Testing/**"
+    static let unitTests: SourceFilesList = "Tests/**"
+    static let uiTests: SourceFilesList = "UITests/**"
+}
