30898b86f34a7424757304303d2b551e4eacba52
changkyu <changkyu@bejewel.kr>
2024-11-21 08:00:58 +0900

    module types 추가

--- appstore/Tuist/ProjectDescriptionHelpers/Modules/ModuleType.swift
+++ appstore/Tuist/ProjectDescriptionHelpers/Modules/ModuleType.swift
@@ -5,4 +5,22 @@
 //  Created by 박창규 on 11/21/24.
 //
 
-import Foundation
+import ProjectDescription
+
+public enum ModuleType {
+    case interface
+    case implementation
+    case testing
+    case tests
+    case demo
+    
+    var name: String {
+        switch self {
+        case .interface: return "Interface"
+        case .implementation: return ""
+        case .tests: return "Tests"
+        case .testing: return "Testing"
+        case .demo: return "Demo"
+        }
+    }
+}
--- appstore/Tuist/ProjectDescriptionHelpers/Modules/SharedModule.swift
+++ appstore/Tuist/ProjectDescriptionHelpers/Modules/SharedModule.swift
@@ -5,4 +5,17 @@
 //  Created by 박창규 on 11/21/24.
 //
 
-import Foundation
+import ProjectDescription
+// TQ -> TalQ
+
+public enum SharedModule: String, CaseIterable {
+    case TQCommonUI
+    case TQDesignSystem
+    case TQFeatureFlag
+    case TQFoundation
+    case TQUIKit
+    
+    public var name: String {
+        self.rawValue
+    }
+}
