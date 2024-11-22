7867fda24c846e4b7b22acd79bd7e5bfe83cbb71
changkyu <changkyu@bejewel.kr>
2024-11-22 20:02:45 +0900

    plugin연결

--- appstore/Tuist/Config.swift
+++ appstore/Tuist/Config.swift
@@ -1,9 +1,8 @@
 import ProjectDescription
 
 let config = Config(
-//    Create an account with "tuist auth" and a project with "tuist project create"
-//    then uncomment the section below and set the project full-handle.
-//    * Read more: https://docs.tuist.io/guides/quick-start/gather-insights
-//
-//    fullHandle: "{account_handle}/{project_handle}",
+    plugins: [
+        .local(path: .relativeToRoot("Plugin/TemplatePlugin"))
+    ],
+    generationOptions: .options()
 )
