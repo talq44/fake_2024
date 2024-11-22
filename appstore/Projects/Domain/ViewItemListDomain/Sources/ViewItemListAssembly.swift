700fad7345ea9fa5ea65d2a2dfd3ee65dac886af
changkyu <changkyu@bejewel.kr>
2024-11-23 07:20:59 +0900

    [#52] 구현부 추가

--- appstore/Projects/Domain/ViewItemListDomain/Sources/ViewItemListAssembly.swift
+++ appstore/Projects/Domain/ViewItemListDomain/Sources/ViewItemListAssembly.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+import AnalyticsCoreInterface
+import ViewItemListDomainInterface
+
+import Swinject
+
+final public class ViewItemListAssembly: Assembly {
+    public init() {}
+    public func assemble(container: Container) {
+        container.register(ViewItemListUseCase.self) { r in
+            let analytics = r.resolve(AnalyticsSend.self)!
+            return ViewItemListUseCaseImpl(analytics: analytics)
+        }
+    }
+}
--- appstore/Projects/Domain/ViewItemListDomain/Sources/ViewItemListUseCaseImpl.swift
+++ appstore/Projects/Domain/ViewItemListDomain/Sources/ViewItemListUseCaseImpl.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+import AnalyticsCoreInterface
+import ViewItemListDomainInterface
+
+final class ViewItemListUseCaseImpl: ViewItemListUseCase {
+    private let analytics: AnalyticsCoreInterface.AnalyticsSend
+    
+    init(analytics: AnalyticsCoreInterface.AnalyticsSend) {
+        self.analytics = analytics
+    }
+    
+    func execute(
+        _ viewItemList: ViewItemListDomainInterface.ViewItemList
+    ) {
+        // 0개면 보내지 않는다.
+        guard viewItemList.items.count > 0 else {
+            return
+        }
+        
+        let viewItemList = AnalyticsCoreInterface.ViewItemList(
+            item_list_id: viewItemList.item_list_id,
+            item_list_name: viewItemList.item_list_name,
+            items: viewItemList.items.map { item in
+                return AnalyticsCoreInterface.Item(
+                    item_id: item.item_id,
+                    item_name: item.item_name
+                )
+            }
+        )
+        
+        self.analytics.send(.view_item_list(viewItemList))
+    }
+}
