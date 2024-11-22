7d033b7be9642178d792f274cc8108189577be4e
changkyu <changkyu@bejewel.kr>
2024-11-23 07:09:38 +0900

    [NO-ISSUE] 요청용 모델 정의

--- appstore/Projects/Domain/ViewItemListDomain/Interface/ViewItemList.swift
+++ appstore/Projects/Domain/ViewItemListDomain/Interface/ViewItemList.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+public struct ViewItemList {
+    public let items: [Item]
+    public let item_list_id: String
+    public let item_list_name: String
+    
+    public init(items: [Item], item_list_id: String, item_list_name: String) {
+        self.items = items
+        self.item_list_id = item_list_id
+        self.item_list_name = item_list_name
+    }
+}
