340c8bdbc74e93f881c06047a0b6113ad94d6067
changkyu <changkyu@bejewel.kr>
2024-11-23 07:07:32 +0900

    [NO-ISSUE] 아이템 최소단위 정의

--- appstore/Projects/Domain/ViewItemListDomain/Interface/Item.swift
+++ appstore/Projects/Domain/ViewItemListDomain/Interface/Item.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+public struct Item {
+    public let item_id: String
+    public let item_name: String
+    
+    public init(item_id: String, item_name: String) {
+        self.item_id = item_id
+        self.item_name = item_name
+    }
+}
