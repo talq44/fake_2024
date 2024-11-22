a8a7d398d89e3a405ece5a6030909d9a979b41d8
changkyu <changkyu@bejewel.kr>
2024-11-23 07:30:34 +0900

    [#52] 테스트 영역 추가

--- appstore/Projects/Domain/ViewItemListDomain/Tests/ViewItemListDomainTest.swift
+++ appstore/Projects/Domain/ViewItemListDomain/Tests/ViewItemListDomainTest.swift
@@ -1,11 +1,46 @@
 import XCTest
 
+@testable import ViewItemListDomain
+@testable import ViewItemListDomainTesting
+
 final class ViewItemListDomainTests: XCTestCase {
-    override func setUpWithError() throws {}
+    
+    private var container: StubViewItemListContainer!
+    
+    override func setUpWithError() throws {
+        self.container = StubViewItemListContainer(
+            assemble: ViewItemListAssembly()
+        )
+    }
 
-    override func tearDownWithError() throws {}
+    override func tearDownWithError() throws {
+        self.container = nil
+    }
 
-    func testExample() {
-        XCTAssertEqual(1, 1)
+    func testExample() throws {
+        // given
+        let item_list_id: String = "item_list_id"
+        let item_list_name: String = "item_list_name"
+        let items: [Item] = [
+            Item(item_id: "item_id", item_name: "item_name")
+        ]
+        
+        guard let useCase = self.container.build() as? ViewItemListUseCaseImpl else {
+            XCTFail("useCase가 성공적으로 생성되지 않았습니다.")
+            return
+        }
+        
+        // when
+        useCase.execute(ViewItemList(
+            item_list_id: item_list_id,
+            item_list_name: item_list_name,
+            items: items
+        ))
+        
+        // then
+        XCTAssertTrue(
+            true,
+            "analytics에 성공적으로 값이 전달되어야만 한다."
+        )
     }
 }
