02a4f0e5094e7337a200491ba76f9800b8ad76bb
changkyu <changkyu@bejewel.kr>
2024-11-22 21:04:54 +0900

    파일 추가 및 삭제

---
+++ appstore/Projects/Domain/ScreenViewDomain/Interface/Pages.swift
@@ -0,0 +1,8 @@
+//
+//  Pages.swift
+//  ScreenViewDomainInterface
+//
+//  Created by 박창규 on 11/22/24.
+//
+
+import Foundation
---
+++ appstore/Projects/Domain/ScreenViewDomain/Interface/ScreenViewProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenViewProtocol.swift
+//  ScreenViewDomainInterface
+//
+//  Created by 박창규 on 11/22/24.
+//
+
+import Foundation
