ec887de20c397685b8ab2b9ad4d0d6c91f613833
changkyu <changkyu@bejewel.kr>
2024-11-22 21:08:47 +0900

    [#48] usecase 구현부 추가

---
+++ appstore/Projects/Domain/ScreenViewDomain/Sources/ScreenViewUseCaseImpl.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenViewUseCaseImpl.swift
+//  ScreenViewDomainInterface
+//
+//  Created by 박창규 on 11/22/24.
+//
+
+import Foundation
+
+import ScreenViewDomainInterface
+
+final internal class ScreenViewUseCaseImpl: ScreenViewUseCase {
+    
+    func execute(
+        page: ScreenViewDomainInterface.Pages,
+        screenAnyClass: AnyClass
+    ) {
+        
+    }
+}
