0ff6d552063c37b5e8a2dce0afa33ce3001a9cb8
changkyu <changkyu@bejewel.kr>
2024-11-22 21:31:52 +0900

    가짜 콘테이너 추가

--- appstore/Projects/Domain/ScreenViewDomain/Testing/StubScreenViewUseCaseContainer.swift
+++ appstore/Projects/Domain/ScreenViewDomain/Testing/StubScreenViewUseCaseContainer.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+import AnalyticsCoreInterface
+import AnalyticsCoreTesting
+import ScreenViewDomainInterface
+
+import Swinject
+
+final class StubScreenViewUseCaseContainer {
+    
+    let container: Container
+    
+    init(assemble: Assembly) {
+        self.container = Swinject.Container()
+        
+        _ = Assembler(
+            [
+                StubAnalyticsSendAssembly(),
+                assemble
+            ],
+            container: container
+        )
+    }
+    
+    func build() -> ScreenViewUseCase? {
+        return self.container.resolve(ScreenViewUseCase.self)
+    }
+}
