70681369f8170a817945486e7595a67426df7353
changkyu <changkyu@bejewel.kr>
2024-11-22 21:12:34 +0900

    [#48 ]실제 구현부 선언

--- appstore/Projects/Domain/ScreenViewDomain/Sources/ScreenViewUseCaseImpl.swift
+++ appstore/Projects/Domain/ScreenViewDomain/Sources/ScreenViewUseCaseImpl.swift
@@ -7,14 +7,30 @@
 
 import Foundation
 
+import AnalyticsCoreInterface
 import ScreenViewDomainInterface
 
 final internal class ScreenViewUseCaseImpl: ScreenViewUseCase {
+    
+    private let analytics: AnalyticsCoreInterface.AnalyticsSend
+    
+    init(analytics: AnalyticsCoreInterface.AnalyticsSend) {
+        self.analytics = analytics
+    }
     
     func execute(
         page: ScreenViewDomainInterface.Pages,
         screenAnyClass: AnyClass
     ) {
+        let className = NSStringFromClass(screenAnyClass)
+        let components = className.components(separatedBy: ".")
+        let screenClass: String = components.last ?? "unknown"
         
+        let screenView = ScreenView(
+            screen_class: screenClass,
+            screen_name: page.rawValue
+        )
+        
+        self.analytics.send(AnalyticsEvent.screen_view(screenView))
     }
 }
