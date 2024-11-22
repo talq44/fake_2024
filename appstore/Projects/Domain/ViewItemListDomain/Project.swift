767fe90601b6bd7ddd93e5fbd37811e5d8392ec4
changkyu <changkyu@bejewel.kr>
2024-11-23 07:03:09 +0900

    [NO-ISSUE] 도메인 모듈 구성

---
+++ appstore/Projects/Domain/ViewItemListDomain/Project.swift
@@ -0,0 +1,36 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: DomainModule.ViewItemListDomain.name,
+    settings: .Module.default,
+    targets: [
+		.interface(
+			domain: .ViewItemListDomain,
+			dependencies: []
+		),
+		.implementation(
+			domain: .ViewItemListDomain,
+			dependencies: [
+                .SPM.swinject,
+                .core(target: .AnalyticsCore, type: .interface),
+				.domain(target: .ViewItemListDomain, type: .interface),
+			]
+		),
+		.testing(
+			domain: .ViewItemListDomain,
+			dependencies: [
+                .SPM.swinject,
+                .core(target: .AnalyticsCore, type: .testing),
+				.domain(target: .ViewItemListDomain, type: .interface),
+			]
+		),
+		.tests(
+			domain: .ViewItemListDomain,
+			dependencies: [
+				.domain(target: .ViewItemListDomain, type: .implementation),
+				.domain(target: .ViewItemListDomain, type: .testing),
+			]
+		),
+]
+)
