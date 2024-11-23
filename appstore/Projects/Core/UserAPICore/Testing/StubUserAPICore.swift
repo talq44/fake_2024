911075b99ece3ef93f11a07f11004e8b105df030
changkyu <changkyu@bejewel.kr>
2024-11-23 14:28:35 +0900

    [#4] Stub 데이터 추가

--- appstore/Projects/Core/UserAPICore/Testing/StubUserAPICore.swift
+++ appstore/Projects/Core/UserAPICore/Testing/StubUserAPICore.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+import UserAPICoreInterface
+
+final class StubUserAPICore: UserAPI {
+    
+    private var searchWords: Set<String> = []
+    
+    func get_search(
+        request: UserAPICoreInterface.SearchRequestDTO
+    ) async throws -> UserAPICoreInterface.SearchResponseDTO {
+        do {
+            let response = try JSONDecoder().decode(
+                SearchResponseDTO.self,
+                from: SearchResponseDTO.mockJson().data(using: .utf8)!
+            )
+            return response
+        } catch {
+            throw error
+        }
+    }
+    
+    func get_lookup(
+        request: UserAPICoreInterface.LookupRequestDTO
+    ) async throws -> UserAPICoreInterface.LookupResponseDTO {
+        do {
+            let response = try JSONDecoder().decode(
+                LookupResponseDTO.self,
+                from: LookupResponseDTO.mock().data(using: .utf8)!
+            )
+            return response
+        } catch {
+            throw error
+        }
+    }
+    
+    func get_searchs_word() async throws -> [String] {
+        return Array(self.searchWords)
+    }
+    
+    func post_searchs_word(word: String) {
+        guard !self.searchWords.contains(word) else {
+            return
+        }
+        self.searchWords.insert(word)
+    }
+}
