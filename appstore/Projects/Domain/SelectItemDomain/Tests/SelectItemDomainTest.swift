e93a6685c88cc4064ab2f08849a3de4301499e50
changkyu <changkyu@bejewel.kr>
2024-11-23 09:06:55 +0900

    [#8] 테스트 코드 추가

--- appstore/Projects/Domain/SelectItemDomain/Tests/SelectItemDomainTest.swift
+++ appstore/Projects/Domain/SelectItemDomain/Tests/SelectItemDomainTest.swift
@@ -1,11 +1,36 @@
 import XCTest
 
+@testable import SelectItemDomain
+@testable import SelectItemDomainTesting
+
 final class SelectItemDomainTests: XCTestCase {
-    override func setUpWithError() throws {}
-
-    override func tearDownWithError() throws {}
-
-    func testExample() {
-        XCTAssertEqual(1, 1)
+    
+    private var container: StubSelectItemDomainContainer!
+    
+    override func setUpWithError() throws {
+        self.container = StubSelectItemDomainContainer(
+            assemble: SelectItemDomainAssembly()
+        )
+    }
+    
+    override func tearDownWithError() throws {
+        self.container = nil
+    }
+    
+    func testExample() throws {
+        // given
+        
+        // when
+        guard let useCase = self.container.build() as? SelectItemUseCaseImpl else {
+            XCTFail("useCase가 성공적으로 생성되지 않았습니다.")
+            return
+        }
+        
+        // then
+        
+        XCTAssertTrue(
+            true,
+            "analytics에 성공적으로 값이 전달되어야만 한다."
+        )
     }
 }
