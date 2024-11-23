1736ff2ea7713d22cc95a91be08727e0c0d015e4
changkyu <changkyu@bejewel.kr>
2024-11-23 17:13:37 +0900

    [#7] interface 영역 파일 선언

---
+++ appstore/Projects/Domain/DetailPageDomain/Interface/DetailPageInput.swift
@@ -0,0 +1,8 @@
+//
+//  DetailPageInput.swift
+//  DetailPageDomainInterface
+//
+//  Created by 박창규 on 11/23/24.
+//
+
+import Foundation
---
+++ appstore/Projects/Domain/DetailPageDomain/Interface/DetailPageOutput.swift
@@ -0,0 +1,8 @@
+//
+//  DetailPageOutput.swift
+//  DetailPageDomainInterface
+//
+//  Created by 박창규 on 11/23/24.
+//
+
+import Foundation
