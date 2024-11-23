5ae8a57226bc762e8d05c4d9b0820b207e8b6a9b
changkyu <changkyu@bejewel.kr>
2024-11-23 15:50:57 +0900

    [#6] entity 정의

--- appstore/Projects/Domain/SearchListDomain/Sources/SearchListEntity.swift
+++ appstore/Projects/Domain/SearchListDomain/Sources/SearchListEntity.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+import SearchListDomainInterface
+
+struct SearchListEntity {
+    let totalCount: Int
+    let items: [Item]
+    let statusCode: Int?
+    
+    struct Item: SearchListItem {
+        let id: String
+        let name: String
+        let averageUserRating: Double
+        let userRatingCount: Int
+        let sellerName: String
+        let categoryName: String
+        let thumbnailURL: String?
+        let screenshotUrls: [String]
+    }
+    
+    init(totalCount: Int, items: [Item]) {
+        self.totalCount = totalCount
+        self.items = items
+        self.statusCode = nil
+    }
+    
+    init (statusCode: Int) {
+        self.totalCount = 0
+        self.items = []
+        self.statusCode = statusCode
+    }
+}
