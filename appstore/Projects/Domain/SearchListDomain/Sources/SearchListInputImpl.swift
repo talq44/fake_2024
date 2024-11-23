01ca790de471abadc489852f936a29d16da9d9ba
changkyu <changkyu@bejewel.kr>
2024-11-23 22:19:49 +0900

    [#6] 의존성 주입 구현

---
+++ appstore/Projects/Domain/SearchListDomain/Sources/SearchListInputImpl.swift
@@ -0,0 +1,8 @@
+//
+//  SearchListInputImpl.swift
+//  SearchListDomainTesting
+//
+//  Created by 박창규 on 11/23/24.
+//
+
+import Foundation
---
+++ appstore/Projects/Domain/SearchListDomain/Testing/StubSearchListContainer.swift
@@ -0,0 +1,8 @@
+//
+//  StubSearchListContainer.swift
+//  SearchListDomainTesting
+//
+//  Created by 박창규 on 11/23/24.
+//
+
+import Foundation
