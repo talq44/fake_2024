caae6baa68a121403cdb0dc7b8ed2c8439068aaf
changkyu <changkyu@bejewel.kr>
2024-11-23 22:20:28 +0900

    [#6] 가짜용 container 추가

--- appstore/Projects/Domain/SearchListDomain/Testing/StubSearchListContainer.swift
+++ appstore/Projects/Domain/SearchListDomain/Testing/StubSearchListContainer.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+import Swinject
+import UserAPICoreTesting
+import SearchListDomainInterface
+
+final class StubSearchListContainer {
+    private let container = Swinject.Container()
+    
+    init(assembly: Assembly) {
+        _ = Assembler(
+            [
+                StubUserAPICoreAssembly(),
+                assembly
+            ],
+            container: container
+        )
+    }
+    
+    func build() -> SearchListUseCase? {
+        return container.resolve(SearchListUseCase.self)
+    }
+}
