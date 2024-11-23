a4b628df86fe1eb114b9f461733fde56da95a3bb
changkyu <changkyu@bejewel.kr>
2024-11-23 15:49:38 +0900

    [#6] 에러 종류 추가

--- appstore/Projects/Domain/SearchListDomain/Interface/SearchListError.swift
+++ appstore/Projects/Domain/SearchListDomain/Interface/SearchListError.swift
@@ -9,5 +9,7 @@ import Foundation
 
 public enum SearchListError: Error, Equatable {
     case restError(statusCode: Int)
+    /// 더보기 요청 전 패치 요청 필요
+    case fetchRequiredBeforeMore
     case unDefined
 }
