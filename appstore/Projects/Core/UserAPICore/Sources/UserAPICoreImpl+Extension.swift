88d6c387396e992a1d521f4becee7c871c8a0051
changkyu <changkyu@bejewel.kr>
2024-11-23 09:59:38 +0900

    [#4] 구현부 수정

--- appstore/Projects/Core/UserAPICore/Sources/UserAPICoreImpl+Extension.swift
+++ appstore/Projects/Core/UserAPICore/Sources/UserAPICoreImpl+Extension.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UserAPICoreInterface
+
+extension UserAPICoreImpl: UserAPICoreInterface.UserAPI {
+    public func get_search(
+        request: SearchRequestDTO
+    ) async throws -> SearchResponseDTO {
+        return try await self.requestJson(
+            .get_search(request: request),
+            type: SearchResponseDTO.self
+        )
+    }
+    
+    public func get_lookup(
+        request: LookupRequestDTO
+    ) async throws -> LookupResponseDTO {
+        return try await self.requestJson(
+            .get_lookup(request: request),
+            type: LookupResponseDTO.self
+        )
+    }
+    
+    private var list_key: String {
+        return "temp_list"
+    }
+    
+    public func get_searchs_word() async throws -> [String] {
+        var list: [String] = []
+        if let beforeList = UserDefaults.standard.array(
+            forKey: list_key
+        ) as? [String] {
+            list = beforeList
+        }
+        return list
+    }
+    
+    public func post_searchs_word(word: String) {
+        var setList: Set<String> = .init()
+        if let beforeList = UserDefaults.standard.array(
+            forKey: list_key
+        ) as? [String] {
+            beforeList.forEach { setList.insert($0) }
+        }
+        setList.insert(word)
+        
+        let list = Array(setList)
+        UserDefaults.standard.set(list, forKey: list_key)
+    }
+}
