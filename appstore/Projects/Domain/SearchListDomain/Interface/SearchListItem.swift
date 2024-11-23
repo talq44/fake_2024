494005b70754954904002b672d76f1c600759961
changkyu <changkyu@bejewel.kr>
2024-11-23 15:51:24 +0900

    [#6] item 정의

--- appstore/Projects/Domain/SearchListDomain/Interface/SearchListItem.swift
+++ appstore/Projects/Domain/SearchListDomain/Interface/SearchListItem.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 public protocol SearchListItem {
-    
+    var id: String { get }
+    var name: String { get }
+    var averageUserRating: Double { get }
+    var userRatingCount: Int { get }
+    var sellerName: String { get }
+    var categoryName: String { get }
+    var thumbnailURL: String? { get }
+    var screenshotUrls: [String] { get }
 }
