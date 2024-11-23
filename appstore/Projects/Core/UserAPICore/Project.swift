ccce3d52fde16da402a1c8930c332d514ffec43b
changkyu <changkyu@bejewel.kr>
2024-11-23 09:12:30 +0900

    [#4] core 모듈 추가

---
+++ appstore/Projects/Core/UserAPICore/Project.swift
@@ -0,0 +1,32 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: CoreModule.UserAPICore.name,
+    settings: .Module.default,
+    targets: [
+		.interface(
+			core: .UserAPICore,
+			dependencies: []
+		),
+		.implementation(
+			core: .UserAPICore,
+			dependencies: [
+				.core(target: .UserAPICore, type: .interface),
+			]
+		),
+		.testing(
+			core: .UserAPICore,
+			dependencies: [
+				.core(target: .UserAPICore, type: .interface),
+			]
+		),
+		.tests(
+			core: .UserAPICore,
+			dependencies: [
+				.core(target: .UserAPICore, type: .implementation),
+				.core(target: .UserAPICore, type: .testing),
+			]
+		),
+]
+)
