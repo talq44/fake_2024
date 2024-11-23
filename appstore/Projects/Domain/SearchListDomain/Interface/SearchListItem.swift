9d3c8fc6e2fd358046ae5db9d1c41d84cfa1ba60
changkyu <changkyu@bejewel.kr>
2024-11-23 15:00:25 +0900

    [#6] interface 선언

---
+++ appstore/Projects/Domain/SearchListDomain/Interface/SearchListItem.swift
@@ -0,0 +1,8 @@
+//
+//  SearchListItem.swift
+//  SearchListDomainInterface
+//
+//  Created by 박창규 on 11/23/24.
+//
+
+import Foundation
---
+++ appstore/Projects/Domain/SearchListDomain/Interface/SearchListResponse.swift
@@ -0,0 +1,8 @@
+//
+//  SearchListResponse.swift
+//  SearchListDomainInterface
+//
+//  Created by 박창규 on 11/23/24.
+//
+
+import Foundation
