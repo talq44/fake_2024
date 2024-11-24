9697463fb4c9be121c50ff88d0d23e324e3412ca
changkyu <changkyu@bejewel.kr>
2024-11-24 09:46:56 +0900

    [#11] component 1차 구성

--- appstore/Projects/Feature/SearchListFeature/Sources/view/ThirdPreviewView.swift
+++ appstore/Projects/Feature/SearchListFeature/Sources/view/ThirdPreviewView.swift
@@ -9,20 +9,77 @@ import UIKit
 
 import TalqDesignSystem
 
+import SnapKit
+import Kingfisher
+
 class ThirdPreviewView: UIView {
+    typealias State = ThirdPreviewState
+    
     enum Metric {
         static let horizontalInset: CGFloat = .horizontalMargin
         static let spacing: CGFloat = .itemSpacing
         static let round: CGFloat = .itemRound
+        static let itemBorder: CGFloat = 1
     }
     
     private let stackView = UIStackView()
+    private let imageView01 = UIImageView()
+    private let imageView02 = UIImageView()
+    private let imageView03 = UIImageView()
 
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
+        self.initView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func initView() {
+        self.addConfigre()
+        self.makeConstraints()
+    }
+    
+    private func addConfigre() {
+        self.stackView.spacing = Metric.spacing
+        self.stackView.axis = .horizontal
+        self.stackView.distribution = .fillEqually
+        self.stackView.addArrangedSubview(imageView01)
+        self.stackView.addArrangedSubview(imageView02)
+        self.stackView.addArrangedSubview(imageView03)
+        
+        [imageView01, imageView02, imageView03].forEach { img in
+            img.layer.borderWidth = Metric.itemBorder
+            img.layer.borderColor = UIColor.lightGray.cgColor
+            img.layer.cornerRadius = Metric.round
+            img.contentMode = .scaleAspectFit
+        }
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(stackView)
+        
+        self.stackView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(Metric.horizontalInset)
+        }
+    }
+    
+    internal func bind(state: State) {
+        self.imageView01.kf.setImage(with: URL(string: state.previewURL01 ?? ""))
+        self.imageView02.kf.setImage(with: URL(string: state.previewURL02 ?? ""))
+        self.imageView03.kf.setImage(with: URL(string: state.previewURL03 ?? ""))
+    }
 }
+
+#Preview("", body: {
+    let view = ThirdPreviewView(
+        frame: CGRect(
+            origin: CGPointZero,
+            size: CGSize(width: 392, height: 696)
+        )
+    )
+    view.bind(state: .mock)
+    return view
+})
