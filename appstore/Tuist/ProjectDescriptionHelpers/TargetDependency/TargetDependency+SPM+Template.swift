31cc2f0681f20363d6ea3c2bc1813479468e0dd2
changkyu <changkyu@bejewel.kr>
2024-11-24 09:58:08 +0900

    사용하지 않는 의존성 삭제

--- appstore/Tuist/ProjectDescriptionHelpers/TargetDependency/TargetDependency+SPM+Template.swift
+++ appstore/Tuist/ProjectDescriptionHelpers/TargetDependency/TargetDependency+SPM+Template.swift
@@ -10,6 +10,7 @@ import ProjectDescription
 public extension TargetDependency {
     
     enum SPM {
+        // DI
         public static let swinject: TargetDependency = .external(name: "Swinject")
         
         // UI
@@ -17,12 +18,8 @@ public extension TargetDependency {
         public static let kingfisher: TargetDependency = .external(name: "Kingfisher")
         
         // Rx
-        public static let rxBlocking: TargetDependency = .external(name: "RxBlocking")
         public static let rxSwift: TargetDependency = .external(name: "RxSwift")
         public static let rxCocoa: TargetDependency = .external(name: "RxCocoa")
-        public static let rxMoya: TargetDependency = .external(name: "RxMoya")
-        public static let rxDataSources: TargetDependency = .external(name: "RxDataSources")
-        public static let rxGesture: TargetDependency = .external(name: "RxGesture")
         public static let reactorKit: TargetDependency = .external(name: "ReactorKit")
         
         // Network
@@ -30,9 +27,6 @@ public extension TargetDependency {
         public static let moya: TargetDependency = .external(name: "Moya")
         
         // Testable
-        public static let quick: TargetDependency = .external(name: "Quick")
-        public static let nimble: TargetDependency = .external(name: "Nimble")
-        public static let snapshotTesting: TargetDependency = .external(name: "SnapshotTesting")
         public static let fakery: TargetDependency = .external(name: "Fakery")
     }
 }
