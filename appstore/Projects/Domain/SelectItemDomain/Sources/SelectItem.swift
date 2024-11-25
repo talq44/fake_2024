c6d64bd407d41ed91cbf3f7de886b90c2cda91a6
changkyu <changkyu@bejewel.kr>
2024-11-25 18:46:43 +0900

    [#67] 빌드 추가

---
+++ appstore/Projects/Domain/SelectItemDomain/Sources/SelectItem.swift
@@ -0,0 +1,8 @@
+//
+//  SelectItem.swift
+//  SelectItemDomain
+//
+//  Created by 박창규 on 11/25/24.
+//
+
+import Foundation
---
+++ appstore/Projects/Domain/SelectItemDomain/Testing/StubAnalyticsSend.swift
@@ -0,0 +1,8 @@
+//
+//  StubAnalyticsSend.swift
+//  SelectItemDomain
+//
+//  Created by 박창규 on 11/25/24.
+//
+
+import Foundation
