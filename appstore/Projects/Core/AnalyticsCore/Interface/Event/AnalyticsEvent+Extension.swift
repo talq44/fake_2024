aa127947c23e84c2d2d42394505086cbe4823ec9
changkyu <changkyu@bejewel.kr>
2024-11-25 21:11:55 +0900

    [#67] 사용하지 않는 파일 삭제

---
+++ appstore/Projects/Core/AnalyticsCore/Interface/Event/AnalyticsEvent+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsEvent+Extension.swift
+//  AnalyticsCoreInterface
+//
+//  Created by 박창규 on 11/25/24.
+//
+
+import Foundation
---
+++ appstore/Projects/Domain/ScreenViewDomain/Testing/StubAnalyticsSend.swift
@@ -0,0 +1,8 @@
+//
+//  StubAnalyticsSend.swift
+//  ScreenViewDomainTesting
+//
+//  Created by 박창규 on 11/25/24.
+//
+
+import Foundation
