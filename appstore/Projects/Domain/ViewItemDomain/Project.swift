2c4dedd2b98e9a41c374320682badcdab84f84f3
changkyu <changkyu@bejewel.kr>
2024-11-25 21:54:21 +0900

    [#9] viewItem 추가

---
+++ appstore/Projects/Domain/ViewItemDomain/Project.swift
@@ -0,0 +1,33 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: DomainModule.ViewItemDomain.name,
+    settings: .Module.default,
+    targets: [
+		.interface(
+			domain: .ViewItemDomain,
+			dependencies: []
+		),
+		.implementation(
+			domain: .ViewItemDomain,
+			dependencies: [
+				.domain(target: .ViewItemDomain, type: .interface),
+                .core(target: .AnalyticsCore, type: .interface),
+			]
+		),
+		.testing(
+			domain: .ViewItemDomain,
+			dependencies: [
+				.domain(target: .ViewItemDomain, type: .interface),
+			]
+		),
+		.tests(
+			domain: .ViewItemDomain,
+			dependencies: [
+				.domain(target: .ViewItemDomain, type: .implementation),
+				.domain(target: .ViewItemDomain, type: .testing),
+			]
+		),
+]
+)
