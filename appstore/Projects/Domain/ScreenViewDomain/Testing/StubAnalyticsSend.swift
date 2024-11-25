732e21b935fa1428f3fdceae2d674ac3c837c455
changkyu <changkyu@bejewel.kr>
2024-11-25 21:12:09 +0900

    [#67] 테스트용 아날리틱스 추가

--- appstore/Projects/Domain/ScreenViewDomain/Testing/StubAnalyticsSend.swift
+++ appstore/Projects/Domain/ScreenViewDomain/Testing/StubAnalyticsSend.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+import AnalyticsCoreInterface
+
+final class StubAnalyticsSend: AnalyticsSend {
+    var sendEvent: AnalyticsCoreInterface.AnalyticsEvent?
+    func send(_ event: AnalyticsCoreInterface.AnalyticsEvent) {
+        self.sendEvent = event
+    }
+    
+    var isSend: Bool {
+        return sendEvent?.name == "screen_view"
+    }
+}
