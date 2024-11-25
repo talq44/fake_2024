0d43628db30681626d36cdab0e3977e23db44f5f
changkyu <changkyu@bejewel.kr>
2024-11-25 21:45:41 +0900

    [#65] 테스트 코드 추가

--- appstore/Projects/Shared/ErrorShared/Sources/RestAPIErrorImpl.swift
+++ appstore/Projects/Shared/ErrorShared/Sources/RestAPIErrorImpl.swift
@@ -9,13 +9,6 @@ import Foundation
 
 import ErrorSharedInterface
 
-enum RestAPIErrorImpl: Error, RestAPIError {
+enum RestAPIErrorImpl: Error, RestAPIError, Equatable {
     case rest(statusCode: Int)
-    
-    var localizedDescription: String {
-        switch self {
-        case .rest(let statusCode):
-            return "status Code \(statusCode) 이슈가 발생해 작업을 완료할 수 없습니다."
-        }
-    }
 }
--- appstore/Projects/Shared/ErrorShared/Tests/ErrorSharedTest.swift
+++ appstore/Projects/Shared/ErrorShared/Tests/ErrorSharedTest.swift
@@ -7,11 +7,29 @@ final class ErrorSharedTests: XCTestCase {
 
     override func tearDownWithError() throws {}
 
-    func testExample() {
-        let error = RestAPIErrorImpl.rest(statusCode: 400)
-        print(error)
-        print(error.localizedDescription)
+    private func checkRestAPIError(statusCode: Int) throws {
+        throw RestAPIErrorImpl.rest(statusCode: statusCode)
+    }
+    
+    func test_given_9999_resterror_when_then_error() throws {
+        // given
+        let statusCode = 9999
         
-        XCTAssertEqual(1, 1)
+        // when
+        do {
+            try checkRestAPIError(statusCode: statusCode)
+            XCTFail("에러를 강제로 발생시켰기 때문에 성공해선 안됩니다.")
+        } catch {
+            guard let error = error as? RestAPIErrorImpl else {
+                XCTFail("에러를 강제로 발생시켰기 때문에 성공해선 안됩니다.")
+                return
+            }
+            
+            // then
+            XCTAssertTrue(
+                error == .rest(statusCode: statusCode),
+                "에러를 강제로 발생시켰기 때문에 성공해선 안됩니다."
+            )
+        }
     }
 }
