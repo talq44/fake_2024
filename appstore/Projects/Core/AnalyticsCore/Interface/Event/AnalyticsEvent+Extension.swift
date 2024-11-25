0aa11778631dd4035821bb8a1a5e145be6643775
changkyu <changkyu@bejewel.kr>
2024-11-25 21:12:39 +0900

    [#67] 이름 얻기 추가

--- appstore/Projects/Core/AnalyticsCore/Interface/Event/AnalyticsEvent+Extension.swift
+++ appstore/Projects/Core/AnalyticsCore/Interface/Event/AnalyticsEvent+Extension.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+public extension AnalyticsEvent {
+    var name: String {
+        return String(describing: self)
+            .components(separatedBy: "(").first ?? ""
+    }
+}
