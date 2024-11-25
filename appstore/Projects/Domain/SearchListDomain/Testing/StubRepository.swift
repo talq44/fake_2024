bd59ebcdbdeb3539e654e37454f945c09576f014
changkyu <changkyu@bejewel.kr>
2024-11-25 20:59:42 +0900

    [#67] 가짜 repository 추가

--- appstore/Projects/Domain/SearchListDomain/Testing/StubRepository.swift
+++ appstore/Projects/Domain/SearchListDomain/Testing/StubRepository.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+import SearchListDomainInterface
+
+final class StubRepository: SearchListRepository {
+    var isError: Bool = false
+    var totalCount: Int = 100
+    var count: Int = 30
+    
+    func request(
+        _ request: any SearchListRequest
+    ) async -> Result<any SearchListEntity, SearchListError> {
+        guard !isError else {
+            return .failure(.unDefined)
+        }
+        
+        let items = Array(1...count).map { id in
+            StubSearchListItem(id: id)
+        }
+        
+        let entity = StubSearchListEntity(
+            totalCount: self.totalCount,
+            items: items
+        )
+        
+        return .success(entity)
+    }
+}
