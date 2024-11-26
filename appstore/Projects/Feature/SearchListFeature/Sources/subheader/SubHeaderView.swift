0ac0b360da37b58ff80b985a8514d281ddb5a55c
changkyu <changkyu@bejewel.kr>
2024-11-26 22:10:18 +0900

    [#11] 서브 헤더 구축 중

---
+++ appstore/Projects/Feature/SearchListFeature/Sources/subheader/SubHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SubHeaderView.swift
+//  SearchListFeature
+//
+//  Created by 박창규 on 11/26/24.
+//
+
+import UIKit
+
+final class SubHeaderView: UIView {
+
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
